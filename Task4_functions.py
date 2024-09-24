c6811e78cf450b74a002db4a95f89d9407cab8c0
Yuliia Khlystun <Yuliia_Khlystun@epam.com>
2024-09-24 11:50:13 +0300

    done

--- Task4_functions.py
+++ Task4_functions.py
@@ -26,35 +26,23 @@ def create_common_dict(dict_list, keys_of_common_dict):
     """Creates a common dictionary aggregating maximal values of keys across input dictionaries."""
     common_dict = {}
     for key in keys_of_common_dict:
-        # Initialize variables for tracking the index of the dictionary and the maximum value associated with the key
         index_of_name_of_key = 0
         max_value_of_key = 0
-        # Counter to track the current dictionary index
         count_all = 1
-        # Counter to track how many dictionaries contain the current key
         count_exist = 0
-        # Iterate through each dictionary again to find the maximum value for the current key
         for dictionary in dict_list:
-            # Check if the current key exists in the dictionary
             if dictionary.get(key) is not None:
-                # Fetch the value associated with the key
                 value_of_key = dictionary.get(key)
-                # Increment counter for dictionaries containing the key
                 count_exist += 1
-                # Update maximum value and its index if this value is greater than the current maximum
                 if value_of_key > max_value_of_key:
                     max_value_of_key = value_of_key
                     index_of_name_of_key = count_all
-            # Increment the dictionary index counter
             count_all += 1
-            # If the key exists in only one dictionary, use the original key name in the common_dict
         if count_exist == 1:
             common_dict[key] = max_value_of_key
-        # If the key exists in multiple dictionaries, append the index of its maximum occurrence
         else:
             name_of_key = key + "_" + str(index_of_name_of_key)
             common_dict[name_of_key] = max_value_of_key
-        # Return the aggregated dictionary containing maximum values (and indexed keys if necessary)
     return common_dict
 def main(a, b):
     list_of_dicts = generate_list_of_dicts(a, b)
