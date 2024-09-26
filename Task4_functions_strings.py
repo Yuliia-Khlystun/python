17120f90162d3041c2ca98bd1a5c649e031204f6
Yuliia Khlystun <Yuliia_Khlystun@epam.com>
2024-09-26 10:36:11 +0300

    done

--- Task4_functions_strings.py
+++ Task4_functions_strings.py
@@ -37,12 +37,12 @@ def main(text):
     return result
 
 text = """homEwork:
-    	tHis iz your homeWork, copy these Text to variable.
+	tHis iz your homeWork, copy these Text to variable. 
 
-    	You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
+	You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
 
-    	it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.
+	it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE. 
 
-    	last iz TO calculate nuMber OF Whitespace characteRS in this Text. caREFULL, not only Spaces, but ALL whitespaces. I got 87."""
+	last iz TO calculate nuMber OF Whitespace characteRS in this Text. caREFULL, not only Spaces, but ALL whitespaces. I got 87."""
 main(text)
 
