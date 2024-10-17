01f01f0294f53c43d46df24c6503426a684326b5
Yuliia Khlystun <Yuliia_Khlystun@epam.com>
2024-10-17 17:10:49 +0300

    done

--- Task6/Task4_functions.py
+++ Task6/Task4_functions.py
@@ -1,7 +1,9 @@
+import csv
 import os
 import random
 import re
 import string
+from collections import Counter
 
 from Classes import News, PrivateAd, RestaurantReview, FromTXT
 
@@ -73,15 +75,15 @@ def add_sentence (text):
     return text
 
 def create_publication_from_input():
-    kind_of_publication = input("Please, enter the type of publication: News, Private ad, Restaurant Review or File_TXT")
+    kind_of_publication = input("Please, enter the number to choose the type of publication: 1 - News, 2 - Private ad, 3- Restaurant Review, 4-File_TXT")
     file_name = input('Please, enter the name of file for publication ')
-    if kind_of_publication == 'News':
+    if kind_of_publication == '1':
         create_news_from_input().publish(file_name)
-    elif kind_of_publication=='Private ad':
+    elif kind_of_publication=='2':
         create_private_ad_from_input().publish(file_name)
-    elif kind_of_publication=='Restaurant Review':
+    elif kind_of_publication=='3':
         create_restaurant_review_from_input().publish(file_name)
-    elif kind_of_publication=='File_TXT':
+    elif kind_of_publication=='4':
         record_index = input('Please, enter the number of records to add to publication (When a positive number is entered, the first records are displayed. '
                              'When a negative number is entered, the last records are displayed. If the number entered exceeds the total number of entries, '
                              'all records are published) ')
@@ -94,6 +96,11 @@ def create_publication_from_input():
         create_publication_from_txt(record_index, input_filename, file_name, file_path)
     else:
         raise ValueError('Type of publication should be News, Private ad or Restaurant Review')
+    repeat = input('Do you want to enter one more publication? Enter 1 if Yes')
+    if repeat=='1':
+        create_news_from_input()
+    else:
+        create_csv(file_name)
 
 def create_news_from_input():
     text = input("Please, enter the text ")
@@ -117,5 +124,35 @@ def create_publication_from_txt(record_index, input_filename, output_filename, f
     d = FromTXT(record_index, input_filename, file_path)
     d.publish(output_filename)
 
+def create_csv(output_file):
+    try:
+        with open (output_file) as publication:
+            text = publication.read()
+        lowercase_text=text.lower()
+        words=re.findall(r'\b\w+\b', lowercase_text)
+        word_count = Counter(words)
+        letters = [char for char in lowercase_text if char.isalpha()]
+        letter_count_all=Counter(letters)
+        upper_letters = [char for char in text if char.isupper()]
+        letter_count_upper = Counter(upper_letters)
+        print(letter_count_all, letter_count_upper)
+        total_letters = sum(letter_count_all.values())
+        with open('word-count.csv', 'w') as csvfile:
+            writer = csv.writer(csvfile, delimiter='-')
+            for word, count in word_count.items():
+                writer.writerow([word, count])
+        with open('letter-count.csv', 'w') as csvfile:
+            headers = ['letter', 'count_all', 'count_uppercase', 'percentage']
+            writer = csv.DictWriter(csvfile, fieldnames=headers)
+            writer.writeheader()
+            for letter, count in letter_count_all.items():
+                if letter_count_upper.get(letter.upper()) is not None:
+                    writer.writerow({'letter': letter, 'count_all': count, 'count_uppercase': letter_count_upper[letter.upper()], 'percentage': int(count/total_letters*100)})
+                else:
+                    writer.writerow({'letter': letter, 'count_all': count, 'count_uppercase': 0, 'percentage': int(count/total_letters*100)})
+        print('CSV files have been created successfully')
+    except IOError:
+        print('Error: File does not exist')
+create_csv('News_feed.txt')
 
 
